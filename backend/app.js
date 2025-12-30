ddb336315591e841d1c4098403f2e42de5aa48dc
tulio-gabriel <tgct@aluno.ifnmg.edu.br>
2025-12-30 10:50:59 -0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app=express()
+app.use(express.json())
+const PORT=3000
+
+app.get('/',(req,res)=>{
+	res.send('ci / cd pipeline ')
+})
+
+app.listen(PORT,()=>{
+	console.log(`Server running on port ${PORT}`)
+})
